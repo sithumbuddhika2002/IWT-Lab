a3ff39c40e5b74d9b014855eeecc92ab5b6c4fec
Sithum Buddhika <165927012+sithumbuddhika2002@users.noreply.github.com>
2024-06-11 00:15:52 +0530

    IWT lab

---
+++ src/js/myScript.js
@@ -0,0 +1,28 @@
+function loadData(data){
+		if (data == "iphone XS"){
+			document.getElementById("pl").innerHTML = "The iPhone XS has a design visually near-identical to the iPhone X but includes upgraded hardware, featuring the A12 Bionic chip built with a 7 nm process.[16] It features a 5.85 inch (149 mm) OLED display (marketed as 5.8 inch) with a resolution of 2436 × 1125 pixels (2.7 megapixels) at 458 ppi and contains dual 12-megapixel rear cameras and one 7-megapixel front-facing camera. The iPhone XS Max features the same hardware and cameras, but has a larger 6.46 inch (164 mm) OLED display (marketed as 6.5 inch) with a resolution of 2688 × 1242 pixels (3.3 megapixels) at 458 ppi and a larger battery (3,174mAh).[17] It was also noted by the media that the XS received a smaller battery than that of the X (dropping to 2,658 mAh from 2,716 mAh).[18] The XS' battery is a new single-cell L-shaped battery, while the iPhone XS Max battery remains two cells like the iPhone X.[19] Additionally, Apple states that the iPhone XS lasts up to 30 minutes longer than iPhone X, while the iPhone XS Max lasts up to 1.5 hours longer than iPhone X.[20]";
+			
+			
+			document.getElementById("imgl").src = "images/xs.jpg";
+		}
+		else if (data == "iphone XS MAX"){
+				document.getElementById("pl").innerHTML = "The iPhone XS has a design visually near-identical to the iPhone X but includes upgraded hardware, featuring the A12 Bionic chip built with a 7 nm process.[16] It features a 5.85 inch (149 mm) OLED display (marketed as 5.8 inch) with a resolution of 2436 × 1125 pixels (2.7 megapixels) at 458 ppi and contains dual 12-megapixel rear cameras and one 7-megapixel front-facing camera. The iPhone XS Max features the same hardware and cameras, but has a larger 6.46 inch (164 mm) OLED display (marketed as 6.5 inch) with a resolution of 2688 × 1242 pixels (3.3 megapixels) at 458 ppi and a larger battery (3,174mAh).[17] It was also noted by the media that the XS received a smaller battery than that of the X (dropping to 2,658 mAh from 2,716 mAh).[18] The XS' battery is a new single-cell L-shaped battery, while the iPhone XS Max battery remains two cells like the iPhone X.[19] Additionally, Apple states that the iPhone XS lasts up to 30 minutes longer than iPhone X, while the iPhone XS Max lasts up to 1.5 hours longer than iPhone X.[20]";
+				
+				document.getElementById("imgl").src = "images/max.jpg";
+			 
+		}
+		else if (data == "iphone 8S PLUS"){
+				document.getElementById("pl").innerHTML = "Just like the iPhone 8, the Apple iPhone 8 Plus sticks to a familiar form factor, with water resistance and built-in stereo speakers, but replaces the metal housing with a glass back to enable wireless charging. It brings along a new, faster Apple A11 Bionic system chip and a display supporting Apple's True Tone display technology. The dual camera at the back now shoots 4K video at up to 60fps and takes portraits with studio lighting effects. The addition of optical image stabilization to the 2X zoom Telephoto camera should translate to clearer images and portraits. iOS 11 runs on it out of the box, introducing augmented reality applications, a redesigned App Store, a new Control Center layout, and a number of UI refinements";
+				
+				document.getElementById("pl").src = "images/plus.jpeg";
+			  
+		}
+		else if (data == "iphone SE"){
+				document.getElementById("pl").innerHTML = "The iPhone SE incorporates the Apple A15 Bionic (5 nm) architecture system on a chip (SoC), with an integrated motion coprocessor and fifth-generation neural engine. It is available in three internal storage configurations: 64 GB, 128 GB, and 256 GB.[2] It has 4 GB of RAM, an increase over the second-generation model's 3 GB of RAM.[13] The third-generation iPhone SE has the same IP67 rating for dust and water resistance as its predecessor.[2] The phone lacks the ultra-wideband features enabled by the U1 chip found in the iPhone 13 and 13 Pro. Despite the phone's smaller size, which may lead to increased thermal throttling, the SE's A15 SoC runs at the same peak CPU frequencies as the iPhone 13. Like its predecessor, the third-generation iPhone SE does not feature a standard 3.5 mm stereo headphone jack and keeps a home button.";
+				
+				document.getElementById("pl").src = "images/se.jpg";
+			   
+		}
+}
+
+   
